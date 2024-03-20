c33500481ff016092df1b5f9cacd7a901fa3f5bd
Victor Lucena <victor.lucenaaa@gmail.com>
2024-03-20 09:44:40 -0300

    style: :lipstick: added placeholder for loading screen on pictures page

--- src/app/components/picture/picture.component.ts
+++ src/app/components/picture/picture.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IApod } from 'src/app/interfaces/iapod';
 import { ApodService } from 'src/app/services/apod.service';
 import { DatePipe } from '@angular/common';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-picture',
@@ -10,13 +11,17 @@ import { DatePipe } from '@angular/common';
 })
 export class PictureComponent implements OnInit {
   apodData!: IApod;
+  isLoading: boolean = true;
 
   constructor(private apodService: ApodService, private datepipe: DatePipe) {}
 
   ngOnInit(): void {
-    this.apodService.getApod().subscribe((response) => {
-      this.apodData = response;
-      console.log(this.apodData);
-    });
+    this.apodService
+      .getApod()
+      .pipe(delay(2500))
+      .subscribe((response) => {
+        this.apodData = response;
+        this.isLoading = false;
+      });
   }
 }
